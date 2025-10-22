9ba2e1c023025e0c377de66a227c5ff3242b4cd7
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-10-22 23:46:37 +0000

    Dialog created

---
+++ src/app/prid/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const padge = () => {
+  return (
+    <div className="flex">
+      <div>dsdsd</div>
+      <div>dsdsd</div>
+      <div>dsdsd</div>
+    </div>
+  );
+};
+
+export default padge;
